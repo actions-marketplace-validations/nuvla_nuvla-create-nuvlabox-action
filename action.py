daf324659d2b88eb36e87754ae583b9eb55878c8
Cristovao Cordeiro <cris@sixsq.com>
2021-08-11 16:43:53 +0200

    fix filter syntax

--- action.py
+++ action.py
@@ -70,7 +70,7 @@ if __name__ == '__main__':
 
     if not args.nuvlabox_release:
         nuvlabox_release = api.get('nuvlabox-release',
-                                    filter="created:desc",
+                                    filter="orderby=created:desc",
                                     last=1).data['resources'][0]['release']
     else:
         nuvlabox_release = api.get(args.nuvlabox_release).data['release']
